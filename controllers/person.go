f70e673fc4892248eaa0e17bfc10f94443be39e4
Fandi A Riyanto <fandi@impacto.id>
2020-10-04 17:34:28 +0700

    dev

--- controllers/person.go
+++ controllers/person.go
@@ -1,45 +1,57 @@
-package controllerPerson
+package controllers
 
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"../models"
 
 	"github.com/gorilla/mux"
 )
 
-var people []Person
-
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	people := models.PersonData()
 	params := mux.Vars(req)
+
+	itemId, _ := strconv.Atoi(params["id"])
 	for _, item := range people {
-		if item.ID == params["id"] {
+		if item.ID == itemId {
+			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Person{})
+	json.NewEncoder(w).Encode(&models.Person{})
 }
 
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	people := models.PersonData()
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(people)
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	people := models.PersonData()
 	params := mux.Vars(req)
-	var person Person
+	var person models.Person
 	_ = json.NewDecoder(req.Body).Decode(&person)
-	person.ID = params["id"]
+	person.ID, _ = strconv.Atoi(params["id"])
 	people = append(people, person)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(people)
 }
 
 func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	people := models.PersonData()
 	params := mux.Vars(req)
+	itemId, _ := strconv.Atoi(params["id"])
 	for index, item := range people {
-		if item.ID == params["id"] {
+		if item.ID == itemId {
 			people = append(people[:index], people[index+1:]...)
 			break
 		}
 	}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(people)
 }
--- models/person.go
+++ models/person.go
@@ -1,13 +1,15 @@
-package modelsPerson
+package models
 
 type Person struct {
-	ID        string   `json:"id,omitempty"`
+	ID        int      `json:"id,omitempty"`
 	Firstname string   `json:"firstname,omitempty"`
 	Lastname  string   `json:"lastname,omitempty"`
 	Address   *Address `json:"address,omitempty"`
 }
 
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
+func PersonData() []Person {
+	var people []Person
+	people = append(people, Person{ID: 1, Firstname: "Nic", Lastname: "Raboy", Address: &Address{City: "Dublin", State: "CA"}})
+	people = append(people, Person{ID: 2, Firstname: "Maria", Lastname: "Raboy"})
+	return people
 }
